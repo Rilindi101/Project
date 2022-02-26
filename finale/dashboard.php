071e7114e947b19483b70046ee1e36b8806157c6
BesnikBegunca <93323159+BesnikBegunca@users.noreply.github.com>
2022-02-26 19:30:16 +0000

    Add files via upload

---
+++ finale/dashboard.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="dashboard.css">
+</head>
+<body>
+
+<div class="first">
+  <div class="foto">
+    <img src="img/topibuton.png" width="75px;" height="70px;">
+    
+  </div>
+  <a href="#about">Accounts</a>
+  <a href="products.php">Products</a>
+  <a href="orders.php">Orders</a>
+</div>
+
+<div class="main">
+  <h2>Dashboard</h2>
+  <p>You are an Administrator!</p>
+  <p>Be Carefull What You Delete or Edit!</p>
+</div>
+   
+</body>
+</html> 
